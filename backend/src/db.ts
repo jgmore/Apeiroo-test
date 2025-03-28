b88255a5afcdcbf2805da4f3bb2ec08f1bf373b0
jgmore <jgmorenete@gmail.com>
2025-03-28 18:00:02 +0100

    Updated host
    Updated the host of the database

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
   user: 'user01',
-  host: 'postgresql://user01:Nji1644fkbzO7VzN2PMjfY7PbsmyZVjT@dpg-cvipfqpr0fns738k5tmg-a.frankfurt-postgres.render.com/apeiroo',
+  host: 'dpg-cvipfqpr0fns738k5tmg-a.frankfurt-postgres.render.com',
   database: 'apeiroo',
   password: 'Nji1644fkbzO7VzN2PMjfY7PbsmyZVjT',
   port: 5432,
